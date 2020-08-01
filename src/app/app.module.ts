0ada42a669912f2ddc2a4866e17d5fce2b5d92ea
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-08-01 17:28:42 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WebinarpageComponent } from './webinar/webinarpage/webinarpage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './webinar/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebinarpageComponent
+    WebinarpageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
